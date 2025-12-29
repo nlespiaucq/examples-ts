f1da2be159b38781829caac29548a062ba6d0702
Nicolas Lespiaucq <n.lespiaucq@betclicgroup.com>
2025-12-29 15:27:09 +0100

    fix steps.d.ts

--- steps.d.ts
+++ steps.d.ts
@@ -1,10 +1,9 @@
 /// <reference types='codeceptjs' />
 type TodosPage = typeof import('./todomvc-tests/pages/todos.page');
-type CustomHelper = import('./todomvc-tests/helpers/custom.helper');
 
 declare namespace CodeceptJS {
   interface SupportObject { I: I, current: any, TodosPage: TodosPage }
-  interface Methods extends Playwright, REST, CustomHelper {}
+  interface Methods extends Playwright {}
   interface I extends WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
